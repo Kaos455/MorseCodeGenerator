09cc23ad53d176513d7db330967393e6535bfa51
markc <markc@DESKTOP-UGBKLE3>
2024-10-12 18:52:52 +0100

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "imgui.h"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "portaudio.h"
+#include "lame/lame.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Everything needed for ImGui, Mp3Lame, and PortAudio is included in this project." << std::endl;
+}
